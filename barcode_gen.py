3e5922cdf030b5fe456b8bb56af366d6f607d06a
Derek Hoogewerf <dbhoogewerf@gmail.com>
2021-06-03 18:20:36 +0530

    Add files via upload

--- barcode_gen.py
+++ barcode_gen.py
@@ -1,6 +1,7 @@
+from os import write
 import sys
 # import Code39 from barcode module
-from barcode import Code39
+from barcode.codex import Code39, Code128
 
 # import ImageWriter to generate an image file
 from barcode.writer import ImageWriter
@@ -8,10 +9,15 @@ from barcode.writer import ImageWriter
 # Make sure to pass the number as string
 number = sys.argv[1]
 
+choice = input("Code 39 or Code 128")
+
 # create an object of EAN13 class and
 # pass the number with the ImageWriter() as the
 # writer
-my_code = Code39(str(number), writer=ImageWriter())
+if(choice == "128"):
+   my_code = Code128(str(number), writer=ImageWriter())
+else:
+   my_code = Code39(str(number), writer=ImageWriter())
 
 # ready barcode
 my_code.save("new_code1")
